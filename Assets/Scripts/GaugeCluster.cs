e0249ad21a0fece26871446d73ec1985be9e053b
Hampter16 <1108192@apps.nsd.org>
2025-04-23 11:23:08 -0700

    CB; GaugeCluster code

--- Assets/Scripts/GaugeCluster.cs
+++ Assets/Scripts/GaugeCluster.cs
@@ -7,13 +7,28 @@ public class GaugeCluster : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        var chassisScript = GaeObject.Find("TChassis").GetComponent<TChassisScript>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        //if (gameObject.GetChild[0].transform.Rotation.z > 
-            // .Rotate uses euler angles!
+        // rotate RPM indicator
+        if (gameObject.GetChild[0].transform.Rotation.z < ((chassisScript.EngineSpeed / chassisScript.motorTopSpeed) * -100F)) {
+            // .Rotate uses euler angles
+            gameObject.GetChild[0].transform.Rotate(0, 0, (20F + chassisScript.EngineAccel / 2F) * Time.deltaTime);
+        } else if (gameObject.GetChild[0].transform.Rotation.z > ((chassisScript.EngineSpeed / chassisScript.motorTopSpeed) * -100F)) {
+            gameObject.GetChild[0].transform.Rotate(0, 0, (-20F + chassisScript.EngineAccel / 2F) * Time.deltaTime);
+        }
+
+        // rotate fuel indicator
+        if (gameObject.GetChild[1].transform.Rotation.z < ((chassisScript.fuelQty / chassisScript.fuelLimit) * -100F)) {
+            // .Rotate uses euler angles
+            gameObject.GetChild[1].transform.Rotate(0, 0, (20F) * Time.deltaTime);
+        } else if (gameObject.GetChild[1].transform.Rotation.z > ((chassisScript.fuelQty / chassisScript.fuelLimit) * -100F)) {
+            gameObject.GetChild[1].transform.Rotate(0, 0, (-20F) * Time.deltaTime);
+        }
+
+
     }
 }
