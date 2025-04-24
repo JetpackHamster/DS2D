f0b54d669ce64a3e74b1a4643c4d6493c9b7fc74
JetpackHamster <jetpack.hamster.16@gmail.com>
2025-04-23 17:59:29 -0700

    gaugecluster debugging in progress

--- Assets/Scripts/GaugeCluster.cs
+++ Assets/Scripts/GaugeCluster.cs
@@ -4,30 +4,33 @@ using UnityEngine;
 
 public class GaugeCluster : MonoBehaviour
 {
+    //script chassisScript;
+    
     // Start is called before the first frame update
     void Start()
     {
-        var chassisScript = GaeObject.Find("TChassis").GetComponent<TChassisScript>();
+        //script chassisScript = GameObject.Find("TChassis").GetComponent<TChassisScript>();
+        Debug.Log(typeof(GameObject.Find("TChassis").GetComponent<TChassisScript>()));
     }
 
     // Update is called once per frame
     void Update()
     {
-        // rotate RPM indicator
-        if (gameObject.GetChild[0].transform.Rotation.z < ((chassisScript.EngineSpeed / chassisScript.motorTopSpeed) * -100F)) {
+        /*// rotate RPM indicator
+        if (gameObject.transform.GetChild(0).transform.Rotation.z < ((chassisScript.EngineSpeed / chassisScript.motorTopSpeed) * -100F)) {
             // .Rotate uses euler angles
-            gameObject.GetChild[0].transform.Rotate(0, 0, (20F + chassisScript.EngineAccel / 2F) * Time.deltaTime);
-        } else if (gameObject.GetChild[0].transform.Rotation.z > ((chassisScript.EngineSpeed / chassisScript.motorTopSpeed) * -100F)) {
-            gameObject.GetChild[0].transform.Rotate(0, 0, (-20F + chassisScript.EngineAccel / 2F) * Time.deltaTime);
+            gameObject.transform.GetChild(0).transform.Rotate(0, 0, (20F + chassisScript.EngineAccel / 2F) * Time.deltaTime);
+        } else if (gameObject.transform.GetChild(0).transform.Rotation.z > ((chassisScript.EngineSpeed / chassisScript.motorTopSpeed) * -100F)) {
+            gameObject.transform.GetChild(0).transform.Rotate(0, 0, (-20F + chassisScript.EngineAccel / 2F) * Time.deltaTime);
         }
 
         // rotate fuel indicator
-        if (gameObject.GetChild[1].transform.Rotation.z < ((chassisScript.fuelQty / chassisScript.fuelLimit) * -100F)) {
+        if (gameObject.transform.GetChild(1).transform.Rotation.z < ((chassisScript.fuelQty / chassisScript.fuelLimit) * -100F)) {
             // .Rotate uses euler angles
-            gameObject.GetChild[1].transform.Rotate(0, 0, (20F) * Time.deltaTime);
-        } else if (gameObject.GetChild[1].transform.Rotation.z > ((chassisScript.fuelQty / chassisScript.fuelLimit) * -100F)) {
-            gameObject.GetChild[1].transform.Rotate(0, 0, (-20F) * Time.deltaTime);
-        }
+            gameObject.transform.GetChild(1).transform.Rotate(0, 0, (20F) * Time.deltaTime);
+        } else if (gameObject.transform.GetChild(1).transform.Rotation.z > ((chassisScript.fuelQty / chassisScript.fuelLimit) * -100F)) {
+            gameObject.transform.GetChild(1).transform.Rotate(0, 0, (-20F) * Time.deltaTime);
+        }*/
 
 
     }
