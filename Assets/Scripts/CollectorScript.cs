f166959e8de051de7c3ee4f0f13fbef4dabf9c1e
Hampter16 <1108192@apps.nsd.org>
2025-04-29 09:11:03 -0700

    fuelCan update 2 and the clones don't work

--- Assets/Scripts/CollectorScript.cs
+++ Assets/Scripts/CollectorScript.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class CollectorScript : MonoBehaviour
 {
+    public GameObject can;
+    private GameObject newCan;
+    float limit;
     // Start is called before the first frame update
     void Start()
     {
-        
+        limit = can.GetComponent<FuelCanScript>().limit;
     }
 
     // Update is called once per frame
@@ -21,13 +24,26 @@ public class CollectorScript : MonoBehaviour
         if (UnityEditor.ArrayUtility.Contains<GameObject>(gameObject.GetComponentInParent<TradeStationScript>().seekedObjs, collision.gameObject)) {
             var TCScript = GameObject.Find("TChassis").GetComponent<TChassisScript>();
             float value = collision.GetComponent<ScrapScript>().value;
+            // if enough capacity for fuel, add it
             if (TCScript.fuelQty < TCScript.fuelLimit - value) {
                 TCScript.fuelQty += value;
             } else {
+                // put excess fuel into cans
+                float excess = value - (TCScript.fuelLimit - TCScript.fuelQty);
                 TCScript.fuelQty += (TCScript.fuelLimit - TCScript.fuelQty);
+                while (excess > limit) {
+                    makeCan(limit);
+                    excess -= limit;
+                }
+                makeCan(excess);
             }
             UnityEditor.ArrayUtility.Remove(ref gameObject.GetComponentInParent<TradeStationScript>().seekedObjs, collision.gameObject);
             GameObject.Destroy(collision.gameObject);
         }
+    }
+    // spawn new fuelcan
+    void makeCan(float value) {
+        newCan = Instantiate(can, transform.position, transform.rotation);
+        newCan.GetComponent<FuelCanScript>().fuelQty = value;
     }
 }
--- Assets/Scripts/FuelCanScript.cs
+++ Assets/Scripts/FuelCanScript.cs
@@ -5,11 +5,12 @@ using TMPro;
 
 public class FuelCanScript : MonoBehaviour
 {
-    GameObject cam;
-    GameObject pointer;
-    GameObject chassis;
+    public GameObject cam;
+    public GameObject pointer;
+    public GameObject chassis;
     public float fuelQty;
     public float fRate;
+    public float limit;
 
     // Start is called before the first frame update
     void Start()
@@ -17,7 +18,7 @@ public class FuelCanScript : MonoBehaviour
         cam = GameObject.Find("Main Camera");
         pointer = GameObject.Find("MouseCrosshair");
         chassis = GameObject.Find("TChassis");
-        gameObject.GetComponentInChildren<TMP_Text>().text = (("" + fuelQty).Substring(0, ("" + fuelQty).IndexOf(".") + 2) + "L");
+        reloadDisplay();
 
     }
 
@@ -26,18 +27,41 @@ public class FuelCanScript : MonoBehaviour
     {
         // detect when player uses the fuelcan
         if(Input.GetKey(KeyCode.F)) {
-            if(Vector3.Distance(pointer.transform.position, transform.position) < 1) {
+            if (Vector3.Distance(pointer.transform.position, transform.position) < 1) {
+                Debug.Log("I have been chosen!!");
                 // if enough fuel left for full flow rate
-                if (fuelQty > fRate * Time.deltaTime) {
+                if (fuelQty > fRate * Time.deltaTime && (chassis.GetComponent<TChassisScript>().fuelLimit - chassis.GetComponent<TChassisScript>().fuelQty) > fRate * Time.deltaTime) {
                     fuelQty -= fRate * Time.deltaTime;
                     chassis.GetComponent<TChassisScript>().fuelQty += fRate * Time.deltaTime;
                 // else transfer remaining
-                } else if (fuelQty > 0) {
+                } else if (fuelQty > 0 && (chassis.GetComponent<TChassisScript>().fuelLimit - chassis.GetComponent<TChassisScript>().fuelQty) > fuelQty) {
                     chassis.GetComponent<TChassisScript>().fuelQty += fuelQty;
                     fuelQty = 0;
                 }
-                gameObject.GetComponentInChildren<TMP_Text>().text = (("" + fuelQty).Substring(0, ("" + fuelQty).IndexOf(".") + 2) + "L");
+                reloadDisplay();
+            }
+        } else if (Input.GetKey(KeyCode.G)) {
+            if(Vector3.Distance(pointer.transform.position, transform.position) < 1) {
+                Debug.Log("I have been chosen!!");
+                // if enough capacity left for full flow rate
+                if ((limit - fuelQty) > fRate * Time.deltaTime && chassis.GetComponent<TChassisScript>().fuelQty > fRate * Time.deltaTime) {
+                    fuelQty += fRate * Time.deltaTime;
+                    chassis.GetComponent<TChassisScript>().fuelQty -= fRate * Time.deltaTime;
+                // else transfer remaining
+                } else if (chassis.GetComponent<TChassisScript>().fuelQty > 0 && (limit - fuelQty) > chassis.GetComponent<TChassisScript>().fuelQty) {
+                    fuelQty += chassis.GetComponent<TChassisScript>().fuelQty;
+                    chassis.GetComponent<TChassisScript>().fuelQty = 0;
+                }
+                reloadDisplay();
             }
         }
+        if (Input.GetKey(KeyCode.Y)) {
+            Debug.Log("Wheeeeeeeeeeeeeee");
+            gameObject.GetComponent<Rigidbody2D>().velocity += Vector2.up;
+        }
+    }
+    void reloadDisplay() {
+        gameObject.GetComponentInChildren<TMP_Text>().text = (("" + fuelQty).Substring(0, ("" + fuelQty).IndexOf(".") + 2) + "L");
     }
 }
+
