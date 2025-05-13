f66f7938699b20a9ae82aeb8ab92b5a8fabddb0b
Hampter16 <1108192@apps.nsd.org>
2025-05-13 14:24:09 -0700

    CB; saveGame code

--- Assets/Scripts/SaveGameScript.cs
+++ Assets/Scripts/SaveGameScript.cs
@@ -25,19 +25,55 @@ public class SaveGameScript : MonoBehaviour
         // open filename prompt to get filename from user
         // if already selected filename, just save to existing (different button "save as")
         if (filename == null) {
-            // send to saveAs to get filename and append extension
+            // TODO: send to saveAs to get filename and append extension
             Debug.Log("Send to saveAs");
         } else {
             filePath = Path.Combine(Application.persistentDataPath, filename);
-            string allData;
-            
-            // get locations of stuctures and applicable data such as tradestation upgradelists
-            // get all gameobjects with terrain tag and save children data?
+            string allData = "";
+            allData += "|structures:"
+            // get locations of stuctures and applicable data such as tradestation upgradelists from terrain parent & terrain
+            for (int i = 0; i < GameObject.Find("TerrainParent").transform.childCount; i++) {
+                for (int j = 0; j < GameObject.Find("TerrainParent").transform.GetChild(i).transform.childCount; j++) {
+                    // add localPosition of each structure // check if readable numbers, maybe convert
+                    allData += "" + GameObject.Find("TerrainParent").transform.GetChild(i).transform.GetChild(j).transform.localPosition + ";";
+                    // if tradestation, add instance specific data (upgradeList)
+                    if (GameObject.Find("TerrainParent").transform.GetChild(i).transform.GetChild(j).transform.name.equals("TradeStation(Clone)")) {
+                        allData += "upgradeList:" + GameObject.Find("TerrainParent").transform.GetChild(i).transform.GetChild(j).GetComponent<TradeStationScript>().upgradeList + ";endobj;";
+                    } else {
+                        allData += "endobj;";
+                    }
+                }
+            }
             // get locations of existing scrap in magneticScrap/Items layer and any other items
+            allData += "|items:";
+            GameObject[] items = GameObject.FindGameObjectsWithTag("MagneticScrap");
+            for (item : items) {
+                // add localPosition and value of each item // check if readable numbers, maybe convert
+                allData += "" + item.transform.localPosition + ";" + item.GetComponent<ScrapScript>().value + ";endobj;"
+            }
             // get location of player vehicle, and state of upgrades, fuel, etc
-            // ensure terrain generation same, maybe save terrain data
+            allData += "|playervehicle:";
+            GameObject TChassis = GameObject.Find("TChassis");
+            var TChassisScript = GameObject.Find("TChassis").GetComponent<TChassisScript>();
+            allData += TChassis.transform.position + ";";
+            allData += TChassisScript.motorTopSpeed + ";";
+            allData += TChassisScript.idleSpeed + ";";
+            allData += TChassisScript.EngineSpeed + ";";
+            allData += TChassisScript.EngineAccel + ";";
+            allData += TChassisScript.fuelQty + ";";
+            allData += TChassisScript.fuelLimit + ";";
+            allData += TChassisScript.motorForce + ";";
+            allData += TChassisScript.maxMotorForce + ";";
+            allData += TChassisScript.minMotorForce + ";";
+            allData += TChassisScript.brakingForce + ";";
+            allData += TChassisScript.fuelUsageMultiplier + ";";
+            allData += TChassisScript.trackWidth + ";";
+            // get crane magnet properties // TODO: verify unprocessed arrays readable
+            allData += TChassis.transform.GetChild(7).GetComponent<CraneMagnetScript>().xyLimits + ";";
+            allData += "endobj;";
+            // TODO: ensure terrain generation same, maybe save terrain data
             
-            // save all above data to file
+            // save all above data to file, might make new file if none exists
             File.WriteAllText(filePath, allData);
         }
         
